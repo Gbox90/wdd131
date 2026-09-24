0229835991a852139312d6c49be32c3673580c59
gbox90 <gigi41210@gmail.com>
2026-09-24 01:26:42 -0400

    Italy location page push

---
+++ scripts/place.js
@@ -0,0 +1,21 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+const temperature = 8;
+const windSpeed = 10;
+
+function calculateWindChill(temp, wind) {
+    return 13.12 + 0.6215 * temp - 11.37 * Math.pow(wind, 0.16) + 0.3965 * temp * Math.pow(wind, 0.16);
+}
+
+const windChill = document.querySelector("#windchill");
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill.textContent = `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
+} else {
+    windChill.textContent = "N/A";
+}
+
